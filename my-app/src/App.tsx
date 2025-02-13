3260a77280d94c07374b9baae52198b3feb9209e
Debbie Leung <dsl2162@columbia.edu>
2025-02-13 14:11:45 -0500

    Created a Projects grid component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ImageCard from './components/Card';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
-import avrHardware from './media/avr_hardware.jpg';
+import Projects from './components/Projects';
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <ImageCard image={avrHardware} title="AVR Hardware" description="AVR Hardware is a hardware project that uses the AVR microcontroller." />
+      <Projects />
     </ThemeProvider>
   );
 }
---
+++ my-app/src/components/Projects.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Container } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import ImageCard from '../reusables/Card';
+import avrHardware from '../media/avr_hardware.jpg';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+const Projects = () => {
+  const projects = [
+    {
+      title: "AVR Hardware",
+      description: "AVR Hardware is a hardware project that uses the AVR microcontroller.",
+      image: avrHardware
+    },
+    {
+      title: "Project 2",
+      description: "Description for project 2",
+      image: avrHardware  // Using same image as placeholder
+    }
+  ];
+
+  return (
+    <Container>
+      <Grid container spacing={3}>
+        {projects.map((project, index) => (
+          <Grid size={4}>
+            <ImageCard {...project} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default Projects;
